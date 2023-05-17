dae67ed4255d0ffa58c3dd5a7976e85ff8e4d408
Mohamed sebai <mohamedseabeai@gmail.com>
2023-05-17 16:59:11 +0300

    added template information

---
+++ js/main.js
@@ -0,0 +1,114 @@
+
+var myHtmlConten = document.getElementById('htmlContent'),
+    
+    myCssContent = document.getElementById('cssContent'),
+    
+    myJavascriptContent = document.getElementById('javascriptContent'),
+    
+    myJsbomdomContent = document.getElementById('jsdombomContent'),
+    
+    myBootstrapContent = document.getElementById('bootstrapContent');
+    
+    
+    
+
+    htmlBtn.onclick = function() {
+        
+        'user strict';
+        
+        myHtmlConten.style.display = "block";
+        this.style.backgroundColor = "#000";
+        myCssContent.style.display = "none";
+        cssBtn.style.backgroundColor = "#333";
+        myJavascriptContent.style.display = "none";
+        javascriptBtn.style.backgroundColor = "#333";
+        myJsbomdomContent.style.display = "none";
+        jsdombomBtn.style.backgroundColor = "#333";
+        mybootstrapContent.style.display = "none";
+        bootstrapBtn.style.backgroundColor = "#333";
+        
+    }
+    
+
+    cssBtn.onclick = function() {
+        
+        'user strict';
+        
+        myHtmlConten.style.display = "none";
+        htmlBtn.style.backgroundColor = "#333";
+        myCssContent.style.display = "block";
+        this.style.backgroundColor = "#000";
+        myJavascriptContent.style.display = "none"
+        javascriptBtn.style.backgroundColor = "#333"
+        myJsbomdomContent.style.display = "none"
+        jsdombomBtn.style.backgroundColor = "#333"
+        myBootstrapContent.style.display = "none"
+        bootstrapBtn.style.backgroundColor = "#333"
+        
+        
+    }
+    
+    javascriptBtn.onclick = function() {
+        
+        'user strict';
+        
+        myHtmlConten.style.display = "none";
+        htmlBtn.style.backgroundColor = "#333";
+        myCssContent.style.display = "none";
+        cssBtn.style.backgroundColor = "#333";
+        myJavascriptContent.style.display = "block";
+        this.style.backgroundColor = "#000";
+        myJsbomdomContent.style.display = "none";
+        jsdombomBtn.style.backgroundColor = "#333";
+        myBootstrapContent.style.display = "none";
+        bootstrapBtn.style.backgroundColor = "#333";
+        
+    }
+    
+    jsdombomBtn.onclick = function() {
+        
+        'user strict';
+        
+        
+        myHtmlConten.style.display = "none";
+        htmlBtn.style.backgroundColor = "#333";
+        myCssContent.style.display = "none";
+        cssBtn.style.backgroundColor = "#333";
+        myJavascriptContent.style.display = "none";
+        javascriptBtn.style.backgroundColor = "#333";
+        myJsbomdomContent.style.display = "block";
+        this.style.backgroundColor = "#000";
+        myBootstrapContent.style.display = "none";
+        bootstrapBtn.style.backgroundColor = "#333";
+        
+    }
+    
+    
+    bootstrapBtn.onclick = function() {
+        
+        'user strict';
+        
+        
+        myHtmlConten.style.display = "none";
+        htmlBtn.style.backgroundColor = "#333";
+        myCssContent.style.display = "none";
+        cssBtn.style.backgroundColor = "#333";
+        myJavascriptContent.style.display = "none";
+        javascriptBtn.style.backgroundColor = "#333";
+        myJsbomdomContent.style.display = "none";
+        jsdombomBtn.style.backgroundColor = "#333";
+        myBootstrapContent.style.display = "block";
+        this.style.backgroundColor = "#000";
+        
+    }
+
+    
+
+
+
+
+    
+
+
+
+
